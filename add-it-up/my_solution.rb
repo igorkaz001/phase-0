5ffb9003fabf45ec1efc4909781bdc8a5708ac5e
Igor Kazimirov <igor.kaz001@gmail.com>
2015-10-25 20:13:59 -0700

    pre-refactor solution

---
+++ add-it-up/my_solution.rb
@@ -0,0 +1,50 @@
+# Add it up!
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+# I worked on this challenge [by myself, with: ].
+# with Clinton Weber
+
+# 0. total Pseudocode
+# make sure all pseudocode is commented out!
+
+# Input: array
+# Output: sum of array
+# Steps to solve the problem.
+# define a method called total with argument of array
+# add all numbers inside the array
+# output the sum
+
+# 1. total initial solution
+#array_1 = [4, 4, 5, 5, 6, 6, 6, 7] 
+
+def total (array)
+	sum=0
+	array.each do |n|  sum += n
+	end
+	return sum
+end
+
+
+#total (array_1)
+
+
+
+# 3. total refactored solution
+
+
+
+# 4. sentence_maker pseudocode
+# make sure all pseudocode is commented out!
+# Input:
+# Output:
+# Steps to solve the problem.
+
+
+# 5. sentence_maker initial solution
+
+
+
+# 6. sentence_maker refactored solution
