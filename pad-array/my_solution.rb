423b5d8e7926d87f1f8f5bd266f085ef1f0f3250
Igor Kazimirov <igor.kaz001@users.noreply.github.com>
2015-11-02 17:09:43 -0800

    add pad array and calculate mode

--- pad-array/my_solution.rb
+++ pad-array/my_solution.rb
@@ -35,16 +35,21 @@ end
   
 =begin
 Were you successful in breaking the problem down into small steps?
+Although we thought we broke the steps down into small parts, we found that our final product deviated from our planned process. 
 
 Once you had written your pseudocode, were you able to easily translate it into code? What difficulties and successes did you have?
+We were not able to easily translate our pseudocode into code. The diffulty was finding the methods in hash class that would be useful in this challege. 
 
 Was your initial solution successful at passing the tests? If so, why do you think that is? If not, what were the errors you encountered and what did you do to resolve them?
-
+Our initial solution did not pass all the tests, so we had to change some things around, even between the two methods, which was a form of refactoring. 
 When you refactored, did you find any existing methods in Ruby to clean up your code?
+We stubmled upon a method that allowed us to refactor by accident, while trying to get the destructive method to work. 
 
 How readable is your solution? Did you and your pair choose descriptive variable names?
+I hope it's as readable to others as it was for me. We did try to use descriptive variable names while preserving typing efficiency. 
 
 What is the difference between destructive and non-destructive methods in your own words?
+A destructive method will actually alter the variable or the object that is passed into the method. While the non-destructive method will create a temp copy to return, then go back to the original string for any future changes. 
 
 =end
 
--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -67,14 +67,14 @@ def mode(array)
   new_hash = Hash.new(0)
   output = []
   
-  array.each do |i| #iterate through the array
-    new_hash[i] += 1 #using the variable found in the array as a key, and increasing the value by 1 for each occurence
+  array.each do |i| 
+    new_hash[i] += 1 
   end
   p new_hash
   
-  new_hash.each do |i, j|  #iterate through hash assigning each key to i, which is the number from the array and each value to j how many times it is int he array or the counter
-    if new_hash.values.max == j #.values creates an array; .max compares to see if its the largest number; and compares it to j, which is the counter
-      output << i #adds the key to they output array
+  new_hash.each do |i, j|  
+    if new_hash.values.max == j 
+      output << i 
     end
   end
   p output
@@ -87,12 +87,16 @@ end
 # 4. Reflection
 =begin
 Which data structure did you and your pair decide to implement and why?
+We decided to implement the hash structure, without knowing why at first (we saw the hint on the directions page). Later we realized that hash is better for this challange because the key would represent the number from the input array while the value could represent the counter of that key. if we were to use an array, we could not control the index value since it is assigned automaticaly in assending order. 
 
 Were you more successful breaking this problem down into implementable pseudocode than the last with a pair? 
+Unfortunately no. The problem is I do not know all the methods for hash/array class to know what direction we will take in solving the challenge. Once we started trying out differnt things to see what worked did we slowly realize what direction we should go. 
 
 What issues/successes did you run into when translating your pseudocode to code?
+See previous answer. We didn't know which methods we would end up using so pseudocode was failry general. 
 
 What methods did you use to iterate through the content? Did you find any good ones when you were refactoring? Were they difficult to implement?
+For iteration we used both .do and .each method. In our final versin we used combination of .values and .max (which i didnt know before the challange). They were a bit hard to implement becuase it was difficult to conceptualize how they interacted with the hash. once we were able to do that, we implemented them in combination.
 
 
 =end
