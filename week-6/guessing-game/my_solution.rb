16e14bc69f1a1993ceb48515e6fea7a3f4b697c8
Igor Kazimirov <igor.kaz001@users.noreply.github.com>
2015-11-11 22:27:07 -0800

    add final version of guessing game

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -52,6 +52,7 @@
 #       puts "you're wrong"
 #     end
 #   end
+ # end
 # end
 # game = GuessingGame.new(10)
 # game.guess(10)
@@ -74,12 +75,9 @@ class GuessingGame
   def guess(number)
     if number < @answer
       return :low
-      @solved = false
     elsif number > @answer
       return :high
-      @solved = false
     else
-      # @solved = true
       return :correct
     end
     @last_guess = number
@@ -87,35 +85,45 @@ class GuessingGame
 
    def solved?
       if @answer != @last_guess
-        return @solved = false
+        return @solved 
       else
         return @solved = true
       end
     end
-  # def solved?
-  #   if @solved == true
-  #     return true
-  #     # puts "you did it!"
-  #   else
-  #     return false
-  #     # puts "you still haven't guessed it"
-  #   end
-  # end
+  
 end
  # game = GuessingGame.new(10)
  # game.guess(10)
  # game.solved?
 
+=begin
 # Reflection
 
-# How do instance variables and methods represent the characteristics and behaviors (actions) of a real-world object?
+# How do instance variables and methods represent the characteristics and behaviors (actions) of a real-world object? 
+A real-world object that can be used to represent this is a passenger train. The whole train is a class, where each train car is a method. The passengers(local variables) on the train are restricted to their assigned train car, while the conductors(instance variables) can move among all other train cars on the train.  
 
 
 # When should you use instance variables? What do they do for you?
+Instance variables are used to represent something that is importantn enough in the program to be used across all methods. 
 
 
 # Explain how to use flow control. Did you have any trouble using it in this challenge? If so, what did you struggle with?
+I used if/else statment to check against the answer value in the .guess method. Another if/else in the .sovled. 
 
 
 # Why do you think this code requires you to return symbols? What are the benefits of using symbols?
-# 	
+Symbols are useful in making a program more memory efficient, since symbols use less memory, while behaving very simililar to strings. I am not sure why this code required me to return a sybmol. 
+
+
+Note:
+I was able to get the program to work using my own tests, but rspec gave me two errors that I couldn't resolve: 
+1) GuessingGame#guess changes solved? when a correct guess is made
+     Failure/Error: expect { game.guess(10) }.to change(game, :solved?).from(false).to(true)
+       expected #solved? to have changed from false to true, but did not change
+     # ./guessing_game_spec.rb:34:in `block (3 levels) in <top (required)>'
+2) GuessingGame#guess reflects the last guess
+     Failure/Error: expect { game.guess(5) }.to change(game, :solved?).from(true).to(false)
+       expected #solved? to have initially been true, but was false
+     # ./guessing_game_spec.rb:45:in `block (3 levels) in <top (required)>'
+
+=end
