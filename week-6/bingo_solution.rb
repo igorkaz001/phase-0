cbd947e3747fd113be1779e59eba5039f0bfa7e7
Igor Kazimirov <igor.kaz001@users.noreply.github.com>
2015-12-05 12:15:00 -0800

    add bingo solution

--- week-6/bingo_solution.rb
+++ week-6/bingo_solution.rb
@@ -1,3 +1,4 @@
+
 # A Nested Array to Model a Bingo Board SOLO CHALLENGE
 
 # I spent [#] hours on this challenge.
@@ -22,28 +23,57 @@
   #fill in the outline here
 
 # Initial Solution
-
 class BingoBoard
-
+  
   def initialize(board)
     @bingo_board = board
+    @bingo = ["B","I","N","G","O"].sample
+    @number = rand(1..100)
   end
 
-
+  def bingo
+    @bingo_board.each do |array|
+      check = array.index(@number)
+      # p check
+      array[check] = 'X' unless check.nil?      
+    end
+  end
 end
-
-# Refactored Solution
-
-
-
+  
 #DRIVER CODE (I.E. METHOD CALLS) GO BELOW THIS LINE
-board = [[47, 44, 71, 8, 88],
+board = [[47, 44, 71, 8, 88], 
         [22, 69, 75, 65, 73],
         [83, 85, 97, 89, 57],
-        [25, 31, 96, 68, 51],
+        [25, 31, 96, 68, 51], 
         [75, 70, 54, 80, 83]]
 
 new_game = BingoBoard.new(board)
+new_game.bingo.map { |game_board| puts game_board.inspect }
+    
+#     @bingo_board.each { |row|
+#   row.each { |column|
+#       puts column  
+#   }
+# }
+# end
+# end
+  
+  
+# if @bingo == "B"
+#   #check every first number in the array
+#   @bingo_board[0][0] == @number
+  
+#   @bingo_board.each do |row|
+# elsif @bingo == "I"
+#   check every second number in the array
+  
+  # Refactored Solution
+  
+  
+  
 
 
-#Reflection
+
+
+
+ 
