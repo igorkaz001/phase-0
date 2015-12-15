698fff6e6b7786dedb82c38e5bacf25630a79c59
Igor Kazimirov <igor.kaz001@users.noreply.github.com>
2015-12-14 23:22:34 -0800

    add eloquent

---
+++ week-7/eloquent.js
@@ -0,0 +1,42 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+
+// Program Structure
+// Write your own variable and do something to it.
+var igor = "i" + "gor"
+
+
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+var food = prompt("what is your favorite food?");
+alert("Hey! Thats my favorite food too");
+
+
+// Functions
+
+// Complete the `minimum` exercise.
+for (var i = 1; i <= 100; i++)
+		{  if (i % 15 == 0)
+			colsole.log("FizzBuzz");
+			else if (i % 3 == 0)
+			console.log("fizz");
+			else if (i % 5 == 0)
+			console.log("buzz");
+			else console.log(i);
+		}
+
+
+var min = function(a,b){
+	return (a <= b ? a : b);
+}
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+var me = new object ();
+me.name = "Igor";
+me.age = "26";
+me.foods = "pizza", "hot dogs", "ice cram";
+me.quirk = "the grammar in this file is terrible";
+
